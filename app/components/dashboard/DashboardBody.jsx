3bcaa387d2f57fe930c9b507433b0f83d80efdc4
theprakashkumar <prakashcodes@outlook.com>
2023-05-31 14:52:55 +0530

    feat: add dashboard header and card

--- app/components/dashboard/DashboardBody.jsx
+++ app/components/dashboard/DashboardBody.jsx
@@ -1,7 +1,14 @@
+import DashboardCards from "./DashboardCards";
+import Header from "./Header";
 import styles from "./dashboardBody.module.css";
 
 const DashboardBody = () => {
-  return <div className={styles.dashboardBodyContainer}>s</div>;
+  return (
+    <div className={styles.dashboardBodyContainer}>
+      <Header />
+      <DashboardCards />
+    </div>
+  );
 };
 
 export default DashboardBody;
