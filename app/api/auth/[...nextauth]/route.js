4ca7792dc6702aaf245d040d00fecfe72831614f
theprakashkumar <prakashcodes@outlook.com>
2023-06-01 12:15:28 +0530

    feat: add public key

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     signIn: "/signin",
   },
